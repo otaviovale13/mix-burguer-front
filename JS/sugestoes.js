68e806f732cc2eba4e0a0a5156b64fe5751ce566
Mborborema7 <matheusea8@gmail.com>
2025-01-31 15:18:32 -0300

    alterações

--- JS/sugestoes.js
+++ JS/sugestoes.js
@@ -6,3 +6,14 @@ function btn(){
 function voltarHome(){
     window.location.href = "/index.html"
 }
+function btnSacola(){
+    window.location.href = "/carrinho.html"
+  }
+  
+  function btnCardapio(){
+      window.location.href = "/pedido.html"
+    }
+  
+    function irSugestão(){
+      window.location.href = "/sugestoes.html"
+    }
