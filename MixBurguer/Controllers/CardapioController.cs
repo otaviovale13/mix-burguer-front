9c2ad7c83189de82423ea6fd01009be900e07b18
unknown <henriquevale55@gmail.com>
2025-02-10 12:27:01 -0300

    teste

---
+++ MixBurguer/Controllers/CardapioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MixBurguer.Controllers
+{
+    public class CardapioController : Controller
+    {
+        public IActionResult Cardapio()
+        {
+            return View();
+        }
+    }
+}
